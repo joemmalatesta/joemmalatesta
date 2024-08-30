919bbb65314bdd552a77ea227eb9a971b4e46556
Joe <joemmalatesta@gmail.com>
2024-08-30 01:15:47 -0400

    WIP. prolly remove these hashes but they cool

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -18,14 +18,15 @@
 			<h1 class="text-6xl font-semibold text-indigo-300">Joe Malatesta</h1>
 
 			<div class="" transition:fly={{ y: 50, duration: 400, delay: 100 }}>
-				<p>I'm a full stack developer and full time student focused on constantly learning and improving in all apsects of my life. Initally introduced by automation software in highschool, I've learned many types of development languages and tools and continue to do so in work, school, and hobby every single day.
-                </p>
+				<p>I'm a <span class="text-indigo-300">full stack developer</span> and <span class="text-indigo-300">full time student</span> focused on constantly learning and improving in all apsects of my life. Initally introduced by automation software in highschool, I've learned many types of development languages and tools and continue to do so in work, school, and hobby every single day.</p>
 			</div>
 		</section>
 
 		<!-- Updates timeline -->
-		<section transition:fly={{ y: 50, duration: 400, delay: 400 }} class="py-5">
-			<h2 class="text-3xl font-medium text-indigo-300">Updates</h2>
+		<section id="updates" transition:fly={{ y: 50, duration: 400, delay: 400 }} class="py-5">
+			<h2  class="text-3xl font-light py-2 group flex gap-1 items-center border-b-2 border-light/10 relative">Updates
+                <a href="#updates" class="absolute -left-7 text-light/30 rounded-lg opacity-0 group-hover:opacity-100 transition-opacity duration-300">#</a>
+            </h2>
 			{#each updates as update, index}
 				<Timeline {update} {index} />
 			{/each}
@@ -33,12 +34,27 @@
 
 		<!-- Projects section -->
 		<section transition:fly={{ y: 50, duration: 400, delay: 1000 }} class="py-5">
-			<a href="/projects" class="text-3xl font-medium py-2 text-indigo-300">Projects</a>
-			<div class="flex flex-col gap-6">
+			<a href="/projects" class="text-3xl font-light py-2 group flex gap-1 items-center border-b-2 border-light/10">
+                <p>Projects</p>
+                <img src="icons/arrow-right.svg" class="w-6 h-6 group-hover:translate-x-2 transition-all duration-300" alt="arrow right">
+            </a>
+			<div class="flex flex-col gap-6 pt-3">
 				{#each projects as project}
 					<ProjectCard {project} />
 				{/each}
 			</div>
+		</section>
+
+        <!-- Writing section -->
+        <section id="writing" transition:fly={{ y: 50, duration: 400, delay: 400 }} class="py-5">
+            <a href="/writing" class="relative text-3xl font-light py-2 group flex gap-1 items-center border-b-2 border-light/10">
+                <a href="#writing" class="absolute -left-7 text-light/30 rounded-lg opacity-0 group-hover:opacity-100 transition-opacity duration-300">#</a>
+                <p>Writing</p>
+                <img src="icons/arrow-right.svg" class="w-6 h-6 group-hover:translate-x-2 transition-all duration-300" alt="arrow right">
+            </a>			
+            {#each updates as update, index}
+				<Timeline {update} {index} />
+			{/each}
 		</section>
 	{/if}
 </main>
