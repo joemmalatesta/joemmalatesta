19d69b10e40c0b99652642ab7979683fd3c72105
Joe <joemmalatesta@gmail.com>
2024-08-30 10:53:09 -0400

    okay card looks nice.. maybe add some more

--- src/components/Timeline.svelte
+++ src/components/Timeline.svelte
@@ -16,7 +16,7 @@
 <div class="relative pl-8 py-2 group">
 	<!-- Vertical line (::before) ~ Date ~ Title ~ Circle marker (::after) -->
 	<div
-		class="flex flex-col items-start mb-1 group-last:before:hidden before:absolute before:left-2 before:h-full before:px-px before:bg-light before:self-start before:-translate-x-1/2 before:translate-y-3 after:absolute after:left-2 after:w-2 after:h-2 after:bg-dark after:border-4 after:box-content after:border-light after:rounded-full after:-translate-x-1/2 after:translate-y-1.5"
+		class="flex flex-col items-start mb-1 group-last:before:hidden before:absolute before:left-2 before:h-full before:px-px before:bg-light/40 before:self-start before:-translate-x-1/2 before:translate-y-3 after:absolute after:left-2 after:w-[.25rem] after:h-[.25rem] after:bg-light/70 after:border-[3px] after:box-content after:border-light/70 after:rounded-full after:-translate-x-1/2 after:translate-y-2"
 	>
 		<div in:fly={{ x: 100, duration: 300, delay: index * 150 + 500 }}>
 			<time
--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -16,10 +16,24 @@ let ready = false;
     </section>
     
     
-    <section transition:fly={{ y: 50, duration: 400, delay: 200 }} class="flex w-full gap-2 py-10">
-        <img src="/me.webp" class="w-2/5 rounded-lg shadow-md" alt="Joe Malatesta">
-        <p class="w-1/2 text-lg">
+    <section transition:fly={{ y: 50, duration: 400, delay: 200 }} class="flex w-full gap-5 py-10">
+        <div class="flex flex-col gap-0.5 items-center w-2/5">
+
+            <img src="/me.webp" class="rounded-lg " alt="Joe Malatesta">
+            <div class="flex items-center justify-center gap-2 opacity-60 w-full">
+                <img src="icons/map-pin.svg" class="w-4 h-4" alt="location">
+                <p class="text-sm">North Beach, San Francisco</p>
+            </div>
+        </div>
+        <div class="w-3/5 flex flex-col justify-between p-1">
+            <p class="">
             Hi there! I'm Joe, a passionate developer and tech enthusiast. I love creating innovative projects and exploring new technologies. When I'm not coding, you can find me hiking, reading, or experimenting with new recipes in the kitchen.
+        </p>
+        <p class="">
+            I'm a full stack developer with a passion for creating web and mobile applications. I'm currently working on a project that combines my love for technology and cooking.
+        </p>
+        <p class="">
+            I'm a full stack developer with a passion for creating web and mobile applications. I'm currently working on a project that combines my love for technology and cooking.
         </p>
     </section>
     {/if}
