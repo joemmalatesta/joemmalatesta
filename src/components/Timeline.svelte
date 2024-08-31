1819a6df60e1b3674472f236e98ca8b37771b558
Joe <joemmalatesta@gmail.com>
2024-08-31 01:24:08 -0400

    timeline looks cleaner now too

--- src/components/Timeline.svelte
+++ src/components/Timeline.svelte
@@ -16,7 +16,7 @@
 <div class="relative pl-8 py-2 group">
 	<!-- Vertical line (::before) ~ Date ~ Title ~ Circle marker (::after) -->
 	<div
-		class="flex flex-col items-start mb-1 group-last:before:hidden before:absolute before:left-2 before:h-full before:px-px before:bg-light/40 before:self-start before:-translate-x-1/2 before:translate-y-3 after:absolute after:left-2 after:w-[.25rem] after:h-[.25rem] after:bg-light/60 after:border-[3px] after:box-content after:border-light/60 after:rounded-full after:-translate-x-1/2 after:translate-y-2"
+		class="flex flex-col items-start mb-1 group-last:before:hidden before:absolute before:left-2 before:h-full before:px-px before:bg-light/40 before:self-start before:-translate-x-1/2 before:translate-y-3 after:absolute after:left-2 after:w-[.25rem] after:h-[.25rem] after:bg-light/60 after:border-[4px] after:border-light/60 after:rounded-full after:-translate-x-1/2 after:translate-y-2"
 	>
 		<div in:fly={{ x: 50, duration: 300, delay: index * 150 + 300 }}>
 			<time
