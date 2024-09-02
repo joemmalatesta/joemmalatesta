03618535eb600db6fdee03458057d3debae47538
joemmalatesta <joemmalatesta@gmail.com>
2024-09-01 22:42:48 -0400

    add some to the timeline. I'll do this all at another date

--- src/lib/timeline.ts
+++ src/lib/timeline.ts
@@ -7,32 +7,47 @@ export type TimelineItem = {
 export let updates: TimelineItem[] = [
     {
         title: "GrizzHacks 7",
-        date: "Feb 2024",
-        content: "Stepped down to Vice President"
+        date: "Feb 2025",
+        content: "Upcoming... apply today at grizzhacks.org"
     },
     {
         title: "Joined Sentry.io",
         date: "May 2024",
-        content: "I moved out to San Francisco for the summer to join Sentry as a DevRel intern. "
+        content: "Moved to San Francisco. Educated developers for the summer as a DevRel intern"
     },
     {
-        title: "GrizzHacks 6",
-        date: "March 2023",
-        content: "I rebooted GrizzHacks "
+        title: "Rebooted GrizzHacks",
+        date: "March 2024",
+        content: "Rebooted my school's hackathon. Led a team of 12, raised $25k, and hosted over 150 students"
+    },
+    {
+        title: "Won first place at Hack Dearborn",
+        date: "Oct 2023",
+        content: "Built and demoed ScanBite, an app to give a nutritional breakdown of your food from an image"
+    },
+    {
+        title: "Launched Groople.xyz",
+        date: "Jan 2024",
+        content: "Launch a daily word game using AI in a new way."
     },
     {
         title: "Joined Mercedes-Benz Financial",
         date: "May 2023",
-        content: ""
+        content: "Interned as a Software Engineer. Sped up daily processes and built UI for an internal chatbot"
+    },
+    {
+        title: "Joined Alps Alpine",
+        date: "August 2022",
+        content: "Built iOS apps for IoT applications, gaining experience in mobile development and connected devices"
     },
     {
         title: "Graduated High School",
         date: "April 2020",
-        content: "Rochester"
+        content: "Rochester High classic underachiever. Spent my time botting shoe releases and chilling with my psych teacher"
     },
     {
         title: "Born",
         date: "July 2002",
-        content: ""
+        content: "At 1:11pm. No wonder my mother says I'm an angel"
     }
 ];
