e6f51f3f5fc90e5ef38b3240e29a88d9f3f539dd
Joe <joemmalatesta@gmail.com>
2024-09-06 19:48:29 -0400

    padding is correct now

--- src/routes/film/+page.svelte
+++ src/routes/film/+page.svelte
@@ -14,8 +14,10 @@
 <!-- todo: update with new scans when done -->
 <main class="min-h-screen">
 {#if ready}
-<section class="gap-1 py-10" transition:fly={{ y: 50, duration: 400, delay: 0 }}>
+<section class="gap-1 py-5" transition:fly={{ y: 50, duration: 400, delay: 0 }}>
 	<Header title="Film Gallery" description="I've got a film camera. I take pictures when I remember it." />
+    </section>
+    <section class="gap-1" transition:fly={{ y: 50, duration: 400, delay: 200 }}>
 	<div class="grid grid-cols-2 sm:grid-cols-3 gap-4 w-full py-2" transition:fly={{ y: 50, duration: 400, delay: 200 }}>
 		{#each ['birthday', 'brown', 'camerastore', 'concert', 'dancy', 'fishpic', 'grossy', 'mother', 'summercamp'] as picture}
 			<img
