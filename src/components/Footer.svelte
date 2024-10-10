ac754b2713803de57e2b4687dc68453b7a7d1e62
Joe <joemmalatesta@gmail.com>
2024-10-10 17:20:01 -0400

    new linkedin link for some reason

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -6,6 +6,6 @@
         <a class="w-6 h-6 opacity-50 hover:opacity-100 transition-opacity duration-300" href="/Joseph Malatesta.pdf" target="_blank"><img src="/icons/resume.svg" alt="Resume" /></a>
         <a class="w-6 h-6 opacity-50 hover:opacity-100 transition-opacity duration-300" href="https://github.com/joemmalatesta" target="_blank"><img src="/icons/github.svg" alt="GitHub" /></a>
         <a class="w-6 h-6 opacity-50 hover:opacity-100 transition-opacity duration-300" href="https://twitter.com/_joemalatesta" target="_blank"><img src="/icons/twitter.svg" alt="Twitter" /></a>
-        <a class="w-6 h-6 opacity-50 hover:opacity-100 transition-opacity duration-300" href="https://www.linkedin.com/in/joe-malatesta/" target="_blank"><img src="/icons/linkedin.svg" alt="LinkedIn" /></a>
+        <a class="w-6 h-6 opacity-50 hover:opacity-100 transition-opacity duration-300" href="https://www.linkedin.com/in/joemmalatesta/" target="_blank"><img src="/icons/linkedin.svg" alt="LinkedIn" /></a>
     </div>
 </footer>
