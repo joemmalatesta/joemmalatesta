42159c131f4e96fa76077cf1dc4d39cab3f82b72
Joe <joemmalatesta@gmail.com>
2025-05-04 00:07:14 -0400

    cool ish new loading state...

--- src/routes/film/+page.svelte
+++ src/routes/film/+page.svelte
@@ -1,33 +1,53 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
 	import { fly } from 'svelte/transition';
-    import Header from '../../components/Heading.svelte';
-    let ready = false;
+	import Header from '../../components/Heading.svelte';
+	let ready = false;
 	onMount(() => {
 		ready = true;
 	});
 
-
+	let loadedImages: { [key: string]: boolean } = {};
+	function handleImageLoad(picture: string) {
+		loadedImages[picture] = true;
+	}
 </script>
 
 <!-- SECTION FOR FILM PHOTOGRAPHY -->
 <!-- todo: update with new scans when done -->
 <main class="min-h-screen">
-{#if ready}
-<section class="gap-1 py-5" transition:fly={{ y: 50, duration: 400, delay: 0 }}>
-	<Header title="Film Gallery" description="I've got a film camera. I take pictures when I remember it." />
-    </section>
-    <section class="gap-1" transition:fly={{ y: 50, duration: 400, delay: 200 }}>
-	<div class="grid grid-cols-2 sm:grid-cols-3 gap-4 w-full py-2" transition:fly={{ y: 50, duration: 400, delay: 200 }}>
-		{#each ['birthday', 'brown', 'camerastore', 'concert', 'dancy', 'fishpic', 'grossy', 'mother', 'summercamp'] as picture}
-			<img
-				loading="lazy"
-				src="photography/{picture}.webp"
-				alt="film"
-				class="w-full h-40 object-cover rounded-lg ring-2 ring-light/10"
+	{#if ready}
+		<section class="gap-1 py-5" transition:fly={{ y: 50, duration: 400, delay: 0 }}>
+			<Header
+				title="Film Gallery"
+				description="I've got a film camera. I take pictures when I remember it."
 			/>
-		{/each}
+		</section>
+		<section class="gap-1" transition:fly={{ y: 50, duration: 400, delay: 200 }}>
+			<div
+				class="grid grid-cols-2 sm:grid-cols-3 gap-4 w-full py-2"
+				transition:fly={{ y: 50, duration: 400, delay: 200 }}
+			>
+				{#each ['birthday', 'brown', 'camerastore', 'concert', 'dancy', 'fishpic', 'grossy', 'mother', 'summercamp'] as picture}
+					<div class="relative w-full h-40">
+						<div
+							class="absolute inset-0 bg-light/5 rounded-lg ring-2 ring-light/10 z-0 {!loadedImages[
+								picture
+							]
+								? 'animate-pulse'
+								: ''}"
+						></div>
+						<img
+							loading="lazy"
+							src="photography/{picture}.webp"
+							alt="film"
+							on:load={() => handleImageLoad(picture)}
+							class="w-full h-full object-cover rounded-lg ring-2 ring-light/10 relative z-10"
+						/>
+					</div>
+				{/each}
 			</div>
 		</section>
 	{/if}
+	<!-- TODO: make a skeleton loading state with {:else} -->
 </main>
